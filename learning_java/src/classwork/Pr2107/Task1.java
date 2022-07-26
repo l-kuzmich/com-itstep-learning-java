e363189f98c3d054b3f83eaced8ac3a6539f92c3
Lyuba <luba.kuzmich1998@gmail.com>
2022-07-26 13:58:53 +0300

    changes classwork.pr2107 task1

--- learning_java/src/classwork/Pr2107/Task1.java
+++ learning_java/src/classwork/Pr2107/Task1.java
@@ -9,23 +9,21 @@ public class Task1 {
     public int howManyWords (String string){
         int numbersOfWords = 0;
 
+        string = string.trim();
         if (string.length() != 0){
             numbersOfWords++;
-        }
-
-        for (int i = 0; i < string.length(); i++){
-
-            if (string.charAt(i) == ' ' && string.charAt(i + 1) != ' '){
-                numbersOfWords++;
+            for (int i = 0; i < string.length(); i++) {
+                if (string.charAt(i) == ' ' && string.charAt(i + 1) != ' '){
+                    numbersOfWords++;
+                }
             }
-        }
-
+        } else return 0;
         return numbersOfWords;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input the strings: ");
+        System.out.println("Input the string: ");
 
         String words = sc.nextLine();
 
